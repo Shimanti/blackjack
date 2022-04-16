8bc8e38e09dec52f991a725753e89ca812cc373c
Shimanti <shim2022@Shimantis-MBP.hsd1.az.comcast.net>
2022-04-15 22:41:04 -0700

    added evaluation

--- js/main.js
+++ js/main.js
@@ -25,8 +25,6 @@ function deal() {
       let player2Val2 = convertToNum(data.cards[3].value)
       let player1 = player1Val1 + player1Val2
       let player2 = player2Val1 + player2Val2
-      console.log(player1)
-      console.log(player2)
       evaluate(player1, player2)
     })
     .catch(err => {
@@ -34,16 +32,21 @@ function deal() {
     })
   }
 
+  // evaluate cards
   function evaluate(player1, player2) {
-    if (player1 === 21)   {
-      document.querySelector('h3').innerText = "Player1 has Blackjack!!!"
+    let h3Text = document.querySelector('h3')
+    if (player1 === 21 && player2 === 21)   {
+      h3Text.innerText = "Draw - both parties have Blackjack!"
+    } else if (player1 === 21)   {
+      h3Text.innerText = "Player1 has Blackjack!!!"
     } else if (player2 === 21) {
-      document.querySelector('h3').innerText = "House has Blackjack!"
-    } else {
-      document.querySelector('h3').innerText = `Player 1 has ${player1}`
+      h3Text.innerText = "House has Blackjack!"
+    } else  {
+      h3Text.innerText = `Player 1 has ${player1}`
     }
   }
 
+  // convert pictures to numbers
   function convertToNum(val) {
     if(val === "ACE") {
       return 11
